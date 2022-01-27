a052b6f8229c5dc288830cdc3b0bcd6007d7b4ba
techchrism <chris@techchrism.me>
2022-01-26 19:07:58 -0800

    add status websocket

--- ValorantAPIBridge/api/proxy/proxies/StatusAPIProxy.cs
+++ ValorantAPIBridge/api/proxy/proxies/StatusAPIProxy.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
+using System.Net.WebSockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using ValorantAPIBridge.api.proxy;
 
 namespace ValorantAPITest.api.proxy.proxies;
@@ -9,10 +13,13 @@ namespace ValorantAPITest.api.proxy.proxies;
 public class StatusAPIProxy : APIProxy
 {
     private readonly LockfileHandler _lockfileHandler;
+    private readonly List<WebSocket> _listeningWebsockets = new();
 
     public StatusAPIProxy(LockfileHandler lockfileHandler)
     {
         _lockfileHandler = lockfileHandler;
+        _lockfileHandler.LockfileUpdate += HandleLockfileUpdate;
+        _lockfileHandler.LockfileRemove += () => { HandleLockfileUpdate(null); };
     }
 
     public void ProcessContext(HttpListenerContext context, int pathNameArg)
@@ -22,6 +29,13 @@ public class StatusAPIProxy : APIProxy
         {
             context.Response.StatusCode = (int) HttpStatusCode.NotFound;
             context.Response.Close();
+            return;
+        }
+        
+        // Process websocket requests to stream updates
+        if (context.Request.IsWebSocketRequest)
+        {
+            ProcessWebsocketRequest(context);
             return;
         }
         
@@ -40,5 +54,51 @@ public class StatusAPIProxy : APIProxy
     public string GetPathName()
     {
         return "status";
+    }
+
+    private void HandleLockfileUpdate(LockfileData? data)
+    {
+        string lockfileReady = (data == null) ? "false" : "true";
+        var buffer = Encoding.UTF8.GetBytes($"{{\"lockfileReady\": {lockfileReady}}}");
+        
+        // Send to all connected websockets
+        List<Task> sendTasks = new();
+        foreach (var socket in _listeningWebsockets)
+        {
+            if (socket.State == WebSocketState.Open)
+            {
+                sendTasks.Add(socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None));
+            }
+        }
+
+        Task.WaitAll(sendTasks.ToArray());
+    }
+    
+    private async void ProcessWebsocketRequest(HttpListenerContext context)
+    {
+        var wsContext = await context.AcceptWebSocketAsync(null);
+        _listeningWebsockets.Add(wsContext.WebSocket);
+        
+        // Wait for websocket closure
+        var buffer = new byte[1024 * 8];
+        while (true)
+        {
+            try
+            {
+                var segment = new ArraySegment<byte>(buffer, 0, buffer.Length);
+                var result = await wsContext.WebSocket.ReceiveAsync(segment, CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    break;
+                }
+            }
+            catch (Exception e)
+            {
+                break;
+            }
+        }
+
+        _listeningWebsockets.Remove(wsContext.WebSocket);
     }
 }
